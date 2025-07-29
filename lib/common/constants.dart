4501d29cf2065387f5d7c0a21ab2e2cbb5cd14c9
mokurin000 <mokurin000@gmail.com>
2025-07-29 14:24:47 +0800

    fix login

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -5,7 +5,7 @@ class AppConstants {
   static const String aesKey = "n7bx6:@Fg_:2;5E89Phy7AyIcpxEQ:R@";  //AES加密算法所用的密钥
   static const String aesIV = ";;KjR1C3hgB1ovXa";  //AES加密的初始化向量
 
-  static const String chimeHost = "ai.sys-all.cn";  //maimai登录服务器
+  static const String chimeHost = "ai.sys-allnet.cn";  //maimai登录服务器
   static const String chimeSalt = "XcW5FW4cPArBXEk4vzKz3CIrMuA5EVVW";  //用于安全功能（如加密或哈希生成）的盐值。盐值是在加密过程中添加到原始数据中以提高安全性的随机数据。
 
   static const String wechatID = "SGWC";
